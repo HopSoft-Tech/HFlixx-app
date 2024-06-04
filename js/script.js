35220b34bba517702dd2ebfc6d377bf7fdc132b3
HopSoft-Tech <ndukelex4@gmail.com>
2024-06-04 03:21:26 +0100

    Added the functionality for the Movie-Details page

--- js/script.js
+++ js/script.js
@@ -57,6 +57,64 @@ async function displayPopularShows() {
   });
 }
 
+// Display Movie Details
+async function displayMovieDetails(id) {
+  const movieId = window.location.search.split("=")[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const movieDetails = document.querySelector("#movie-details");
+
+  movieDetails.innerHTML = ` <div class="details-top">
+          <div>
+             ${
+               movie.poster_path
+                 ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}">`
+                 : `<img src='images/no-image.jpg' class='card-img-top' alt='${movie.title}'>`
+             }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+            </ul>
+            <a href="${
+              movie.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+              movie.budget
+            )}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+              movie.revenue
+            )}</li>
+            <li><span class="text-secondary">Runtime:</span> ${
+              movie.runtime
+            } minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+            ${movie.production_companies
+              .map((company) => `<span>${company.name}</span>`)
+              .join(", ")}
+          </div>
+        </div>`;
+}
+
 // fetch data from TMDb API
 async function fetchAPIData(endpoint) {
   const API_KEY = "584456b0298ab7dcd1e1ad608d920e58";
@@ -91,6 +149,9 @@ function highlightActiveLink() {
   });
 }
 
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 // Initialize App
 function init() {
   // This is for creating the routes
@@ -100,10 +161,10 @@ function init() {
       displayPopularMovies();
       break;
     case "/shows.html":
-       displayPopularShows();
+      displayPopularShows();
       break;
     case "/movie-details.html":
-      console.log("movie-details");
+      displayMovieDetails();
       break;
     case "/tv-details.html":
       console.log("tv-details");
