997606acbc78a26dfbac55dc74735ac070acf8b1
HopSoft-Tech <ndukelex4@gmail.com>
2024-06-04 12:33:56 +0100

    Added the Movie Details Backdrop Image, Fixed the Show Details Page and added the backdrop Image for the shows

--- js/script.js
+++ js/script.js
@@ -58,14 +58,17 @@ async function displayPopularShows() {
 }
 
 // Display Movie Details
-async function displayMovieDetails(id) {
+async function displayMovieDetails() {
   const movieId = window.location.search.split("=")[1];
 
   const movie = await fetchAPIData(`movie/${movieId}`);
 
-  const movieDetails = document.querySelector("#movie-details");
+  // Overlay for Background Image
+  displayBackgroundImage("movie", movie.backdrop_path);
 
-  movieDetails.innerHTML = ` <div class="details-top">
+  const div = document.createElement("div");
+
+  div.innerHTML = ` <div class="details-top">
           <div>
              ${
                movie.poster_path
@@ -113,10 +116,95 @@ async function displayMovieDetails(id) {
               .join(", ")}
           </div>
         </div>`;
+
+  document.querySelector("#movie-details").appendChild(div);
+}
+
+// Display Show Details
+async function displayShowDetails() {
+  const showId = window.location.search.split("=")[1];
+
+  const show = await fetchAPIData(`tv/${showId}`);
+
+  // Overlay for Background Image
+  displayBackgroundImage("tv", show.backdrop_path);
+
+  const div = document.createElement("div");
+
+  div.innerHTML = ` <div class="details-top">
+          <div>
+             ${
+               show.poster_path
+                 ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}">`
+                 : `<img src='images/no-image.jpg' class='card-img-top' alt='${show.name}'>`
+             }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+            </ul>
+            <a href="${
+              show.homepage
+            }" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number of Episodes:</span> ${
+              show.number_of_episodes
+            }</li>
+            <li><span class="text-secondary">Last Episode To Air:</span> ${
+              show.last_episode_to_air.name
+            } </li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+            ${show.production_companies
+              .map((company) => `<span>${company.name}</span>`)
+              .join(", ")}
+          </div>
+        </div>`;
+
+  document.querySelector("#show-details").appendChild(div);
+}
+// Display Backdrop On details page
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement("div");
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${backgroundPath})`;
+  overlayDiv.style.backgroundSize = "cover";
+  overlayDiv.style.backgroundPosition = "center";
+  overlayDiv.style.backgroundRepeat = "no-repeat";
+  overlayDiv.style.height = "100vh";
+  overlayDiv.style.width = "100vw";
+  overlayDiv.style.position = "fixed";
+  overlayDiv.style.top = "0";
+  overlayDiv.style.left = "0";
+  overlayDiv.style.zIndex = "-1";
+  overlayDiv.style.opacity = "0.1";
+
+  if (type === "movie") {
+    document.querySelector("#movie-details").appendChild(overlayDiv);
+  } else {
+    document.querySelector("#show-details").appendChild(overlayDiv);
+  }
 }
 
 // fetch data from TMDb API
 async function fetchAPIData(endpoint) {
+  // Register your key at https://www.themoviedb.org/settings/api and enter here
+  // Only use this for development or very small projects. You should store your key and make requests from a server
   const API_KEY = "584456b0298ab7dcd1e1ad608d920e58";
   const API_URL = `https://api.themoviedb.org/3/${endpoint}?api_key=${API_KEY}&language=en-US`;
 
@@ -167,7 +255,7 @@ function init() {
       displayMovieDetails();
       break;
     case "/tv-details.html":
-      console.log("tv-details");
+      displayShowDetails();
       break;
     case "/search.html":
       console.log("search");
